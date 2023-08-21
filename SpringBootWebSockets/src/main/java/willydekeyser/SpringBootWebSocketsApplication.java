4526aefc45ca09378e271e73b0fdb236b02a136f
willydekeyser <ma074172@skynet.be>
2023-08-21 16:55:39 +0200

    Setup SpringBootWebSockets

---
+++ SpringBootWebSockets/src/main/java/willydekeyser/SpringBootWebSocketsApplication.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSocketsApplication.class, args);
+	}
+
+}
