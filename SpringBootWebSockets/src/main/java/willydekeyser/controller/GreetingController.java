2e5c30375d1a5116acb6e2b0777f6f269e08a422
willydekeyser <ma074172@skynet.be>
2023-08-24 16:06:02 +0200

    Update delay

--- SpringBootWebSockets/src/main/java/willydekeyser/controller/GreetingController.java
+++ SpringBootWebSockets/src/main/java/willydekeyser/controller/GreetingController.java
@@ -14,7 +14,7 @@ public class GreetingController {
 	@MessageMapping("/hello")
 	@SendTo("/topic/greetings")
 	public Greeting greeting(Message message) throws Exception {
-		Thread.sleep(1000); // simulated delay
+		// Thread.sleep(1000); // simulated delay
 		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
 	}
 	
